325643c65863b096a5e0236bcdeab4c596e5ee8d
Darshan Gautam <dsrsangautam@gmail.com>
2022-06-14 13:26:10 +0545

    completed the alert screen

--- src/screen/Alertscreen.js
+++ src/screen/Alertscreen.js
@@ -1,18 +1,103 @@
 import React from 'react';
-import {View, Text, Dimensions, StyleSheet} from 'react-native';
+import {View, Text, Dimensions, StyleSheet, Alert, Button} from 'react-native';
 
 import Layout from '../components/Layout';
+const simpleAlert = () => {
+  Alert.alert('Hello I am simple alert');
+  // Alert.alert(
+  //   'Alert Title',
+  //   'My Alert Msg',
+  //   [
+  //     {
+  //       text: 'Cancel',
+  //       onPress: () => Alert.alert('Cancel Pressed'),
+  //       style: 'cancel',
+  //     },
+  //   ],
+  //   {
+  //     cancelable: true,
+  //     onDismiss: () =>
+  //       Alert.alert(
+  //         'This alert was dismissed by tapping outside of the alert dialog.',
+  //       ),
+  //   },
+  // );
+};
+const alertTitle = () => {
+  Alert.alert('Alert Title', 'My Alert message');
+};
+const cancelAlert = () => {
+  Alert.alert(
+    'Alert Title',
+    'My Alert Msg',
+    [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+    ],
+    // {cancelable: true},
+  );
+};
+const cancelTouchAlert = () => {
+  Alert.alert(
+    'Alert Title',
+    'My Alert Msg',
+    [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+    ],
+    {cancelable: true},
+  );
+};
 const Alertscreen = ({route}) => {
   return (
     <Layout screenHeader={route.params.screenName}>
-      <View>
-        <Text>This is alert screen texthghhhghghg</Text>
+      <View style={style.componentContainer}>
+        <Text style={{fontSize: 14, fontWeight: 'bold'}}>
+          This is the simple alert.
+        </Text>
+        <Button title={'Press me'} onPress={simpleAlert} color={'#2e8bd1'} />
+      </View>
+      <View style={style.componentContainer}>
+        <Text style={{fontSize: 14, fontWeight: 'bold'}}>
+          Alert with title.
+        </Text>
+        <Button title={'Press me'} onPress={alertTitle} color={'#2e8bd1'} />
+      </View>
+      <View style={style.componentContainer}>
+        <Text style={{fontSize: 14, fontWeight: 'bold'}}>
+          Alert with cancel button.
+        </Text>
+        <Button title={'Press me'} onPress={cancelAlert} color={'#2e8bd1'} />
+      </View>
+      <View style={style.componentContainer}>
+        <Text style={{fontSize: 14, fontWeight: 'bold'}}>
+          Alert message dismisses when touched outside.
+        </Text>
+        <Button
+          title={'Press me'}
+          onPress={cancelTouchAlert}
+          color={'#2e8bd1'}
+        />
       </View>
     </Layout>
   );
 };
 
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+const style = StyleSheet.create({
+  componentContainer: {
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'rgb(243,243,243)',
+    borderWidth: 1,
+    borderColor: '#eee',
+    padding: 15,
+    margin: 8,
+  },
+});
 
 export default Alertscreen;
